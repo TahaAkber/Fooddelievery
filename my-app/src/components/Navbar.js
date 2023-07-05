b357b9d3a0f9217635459b5375c0ce3ecf3f8736
Taha Akber <taha.akber007@gmail.com>
2023-07-06 03:59:05 +0500

    Update Navbar.js

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-success">
         <div className="container-fluid">
           <Link className="navbar-brand" href="/">
             Navbar
