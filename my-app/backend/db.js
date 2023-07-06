523da71d0d44b57bd1c22d081ecaa7b68856198e
Taha <taha.akber007@gmail.com>
2023-07-07 01:30:12 +0500

    v4

--- my-app/backend/db.js
+++ my-app/backend/db.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
 const mongoURI =
-  "mongodb+srv://Kolnice:124890@cluster0.pdtge0u.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://Kolnice:124890@cluster0.pdtge0u.mongodb.net/gofoodmern?retryWrites=true&w=majority";
 const mongoDB = async () => {
   try {
     const connect = await mongoose.connect(mongoURI);
 
     if (connect) {
       console.log("Connection Successful");
+      const fetched_data = await mongoose.connection.db.collection(
+        "food_items"
+      );
+      fetched_data.find().toArray(function (err, result) {
+        if (err) console.log(err);
+        else console.log(result);
+      });
     }
   } catch (error) {
     console.log("Error while connecting", error.message);
